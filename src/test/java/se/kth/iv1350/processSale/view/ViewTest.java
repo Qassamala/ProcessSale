36dd7cde393ffe7f649d03c3f8773f5293080206
Abdi <abdigobena@hotmail.com>
2021-05-05 20:56:24 +0200

    Added unitTest for Register

--- src/test/java/se/kth/iv1350/processSale/view/ViewTest.java
+++ src/test/java/se/kth/iv1350/processSale/view/ViewTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import se.kth.iv1350.processSale.controller.Controller;
+import se.kth.iv1350.processSale.integration.Accounting;
 import se.kth.iv1350.processSale.integration.Inventory;
+import se.kth.iv1350.processSale.integration.Printer;
+import se.kth.iv1350.processSale.model.Register;
+import se.kth.iv1350.processSale.model.Store;
 
 /**
  *
@@ -21,7 +25,11 @@ public class ViewTest {
     @BeforeEach
     public void setUp() {
         Inventory inventory = new Inventory();
-        Controller controller = new Controller(inventory);
+        Accounting accounting = new Accounting();
+        Printer printer = new Printer();
+        Register register = new Register();
+        Store store = new Store();
+        Controller controller = new Controller(inventory, accounting, printer, register, store);
         instanceToTest = new View(controller);
         
         printoutBuffer = new ByteArrayOutputStream();
